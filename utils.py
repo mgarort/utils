44530439f106222b474424f48d22ffc2dc61e0f9
mgarort <miwe.gar.or@gmail.com>
2020-04-22 18:47:55 +0200

    Convert input to DataFrame if it is an array

--- utils.py
+++ utils.py
@@ -1,9 +1,12 @@
+import numpy as np
+import pandas as pd
+
 def split_dataframe(df, split_fractions, shuffle=True, random_seed=None, filename_root=None):
 
     """This function takes as input a dataframe and splits it into training, validation and test sets, with
     the option to shuffle it, use a random seed or save them to file for the future.
 
-    :df: dataframe to act on. Assumes that the label is the last column
+    :df: dataframe to act on. Assumes that the label is the last column. If it is not a dataframe but an array, it gets converted
     :split_fractions: list or tuple with 3 fractions for training, validation and test. Must sum up to 1. 
     :shuffle: boolean. Default True.
     :random_seed: random seed for the shuffling. Default no random seed
@@ -13,9 +16,12 @@ def split_dataframe(df, split_fractions, shuffle=True, random_seed=None, filenam
     """
 
     assert sum(split_fractions) == 1, 'split fractions must sum up to 1'
+
+    if type(df) == np.array:
+        df = pd.DataFrame(df)
     
     if shuffle:
-        df = df.sample(frac=1,random_state=ranom_seed)
+        df = df.sample(frac=1,random_state=random_seed)
 
     n_samples = df.shape[0]
     n_train = int(n_samples*split_fractions[0])
