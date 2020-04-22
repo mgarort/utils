2a7f7f32f386cdacdc9b90d338e1bbffddf5ced1
mgarort <miwe.gar.or@gmail.com>
2020-04-22 15:04:31 +0200

    Function to split dataframe

---
+++ utils.py
@@ -0,0 +1,34 @@
+def split_dataframe(df, split_fractions, shuffle=True, random_seed=None, filename_root=None):
+
+    """This function takes as input a dataframe and splits it into training, validation and test sets, with
+    the option to shuffle it, use a random seed or save them to file for the future.
+
+    :df: dataframe to act on. Assumes that the label is the last column
+    :split_fractions: list or tuple with 3 fractions for training, validation and test. Must sum up to 1. 
+    :shuffle: boolean. Default True.
+    :random_seed: random seed for the shuffling. Default no random seed
+    :filename_root: if you add a filename root, the splits obtained will be save with "filename_root"_train.tsv, etc.
+
+    :returns: tuple with dataframes for training, validation and test.
+    """
+
+    assert sum(split_fractions) == 1, 'split fractions must sum up to 1'
+    
+    if shuffle:
+        df = df.sample(frac=1,random_state=ranom_seed)
+
+    n_samples = df.shape[0]
+    n_train = int(n_samples*split_fractions[0])
+    n_val = int(n_samples*split_fractions[1])
+    n_test = int(n_samples*split_fractions[2])
+
+    train = df.loc[:n_train]
+    val = df.loc[n_train:n_val]
+    test = df.loc[n_train+n_val:n_test]
+
+    if filename_root:
+        train.to_csv(filename_root + '_train.tsv', sep='\t')
+        val.to_csv(filename_root + '_val.tsv', sep='\t')
+        test.to_csv(filename_root + '_test.tsv', sep='\t')
+
+    return train, val, test
