0cd94c29dfdf26b7da34da234b2b9d5595f9a93e
Miguel Garcia-Ortegon <mg770@cam.ac.uk>
2020-09-10 16:32:32 +0100

    Changed the return of the functions to compute the output dimensions after the conv3d and the maxpool3d layers, from a dictionary to a tuple with just the size dimensions, as this was easier to use and also more Pythonic (similar to array.shape or df.shape)

--- nn.py
+++ nn.py
@@ -8,8 +8,12 @@ def compute_dims_after_conv3d(x, in_channels, out_channels, kernel_size, stride=
     """This utility function calculates the output dimensions after a 3D convolution.
     - args: It takes exactly the same parameters as the conv3d layer, so that we can pass exactly the same parameter dictionary. This is
             convenient, but note that some parameters (like bias=True) will never be used.
-    returns: a dictionary with the dimensions n (batch size, c_out (channels out, this is specified by the user in the definition of the 
-             3D convolutional layer), d_out (depth out), h_out (height out), w_out (width out)
+    returns: a tuple with the dimensions that the output would have, i.e. a tuple (n_out, c_out, d_out, h_out, w_out), where:
+                    - n_out: batch size (same as n_in, always for every layer)
+                    - c_out: number of channels (specified by the user in the conv3d layer)
+                    - d_out: depth, depends on the kernel size, stride, padding and dilation
+                    - h_out: height, idem
+                    - w_out: width, idem
     """
 
     # Variables to use in computation
@@ -30,15 +34,19 @@ def compute_dims_after_conv3d(x, in_channels, out_channels, kernel_size, stride=
     c_out = out_channels
     d_out, h_out, w_out = np.floor( (np.array([d_in, h_in, w_in]) + 2*padding - dilation*(kernel_size-1) - 1) /stride + 1)
 
-    return {'n_out': int(n_out), 'c_out': int(c_out), 'd_out':int(d_out), 'h_out':int(h_out), 'w_out':int(w_out)}
+    return (int(n_out), int(c_out), int(d_out), int(h_out), int(w_out))
 
 
 def compute_dims_after_maxpool3d(x, kernel_size, stride=None, padding=0, dilation=1, return_indices=False, ceil_mode=False):
     """This utility function calculates the output dimensions after a 3D max pool operation.
     - args: It takes exactly the same parameters as the maxpool3d layer, so that we can pass exactly the same parameter dictionary. This is
             convenient, but note that some parameters (like bias=True) will never be used.
-    returns: a dictionary with the dimensions n (batch size, c_out (channels out, this is specified by the user in the definition of the 
-             3D convolutional layer), d_out (depth out), h_out (height out), w_out (width out)
+    returns: a tuple with the dimensions that the output would have, i.e. a tuple (n_out, c_out, d_out, h_out, w_out), where:
+                    - n_out: batch size (same as n_in, always for every layer)
+                    - c_out: number of channels (same as c_in for the pooling layer)
+                    - d_out: depth, depends on the kernel size, stride, padding and dilation
+                    - h_out: height, idem
+                    - w_out: width, idem
     """
 
     # Variables to use in computation
@@ -62,4 +70,4 @@ def compute_dims_after_maxpool3d(x, kernel_size, stride=None, padding=0, dilatio
     round_mode = np.ceil if ceil_mode else np.floor # Strangely, the maxpool3d layer accepts ceiling instead of flooring
     d_out, h_out, w_out = round_mode( (np.array([d_in, h_in, w_in]) + 2*padding - dilation*(kernel_size-1) - 1) /stride + 1)
 
-    return {'n_out': int(n_out), 'c_out': int(c_out), 'd_out':int(d_out), 'h_out':int(h_out), 'w_out':int(w_out)}
+    return (int(n_out), int(c_out), int(d_out), int(h_out), int(w_out))
