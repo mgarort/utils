abcb7dd3f5763e83f9585802ba590c162d048e39
mgarort <miwe.gar.or@gmail.com>
2020-10-09 06:52:55 +0100

    Change grid line style in nature.mplstyle

--- plots.py
+++ plots.py
@@ -14,7 +14,6 @@ def load_style(stylename):
         return(stylename)
 
     # If stylename is a custom style
-    __import__('pdb').set_trace()
     stylelib_path = os.path.join(os.path.dirname(__file__), 'stylelib')
     custom_stylepaths = glob.glob(os.path.join(stylelib_path,'*.mplstyle'))
     custom_stylenames = [get_stylename_from_stylepath(stylepath) for stylepath in custom_stylepaths]
